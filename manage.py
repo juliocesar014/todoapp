0d522f4e7469995ff805664db87fb18b39b19c42
Júlio César Agripino de Oliveira <105388145+juliocesar014@users.noreply.github.com>
2023-05-25 19:41:31 -0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -20,3 +20,21 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
+    
+"""
+medias = []
+
+for i in range(10):
+    notas = []
+    for j in range(4):
+        nota = float(input(f'Digite a nota {j+1} do aluno {i+1}: '))
+        notas.append(nota)
+    media = sum(notas) / len(notas)
+    medias.append(media)
+
+alunos_aprovados = sum(1 for media in medias if media > 7)
+
+print(f'{alunos_aprovados} aluno(s) com médias maiores que 7.')
+
+"""
